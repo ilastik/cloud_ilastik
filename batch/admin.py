40880ca18fbdafb28641743bfb921d7f1cc76749
Maksim Novikov <mnovikov.work@gmail.com>
2020-01-14 12:11:54 +0100

    Add job model

--- batch/admin.py
+++ batch/admin.py
@@ -6,3 +6,8 @@ from . import models
 @admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ["file__name"]
+
+
+@admin.register(models.Job)
+class JobAdmin(admin.ModelAdmin):
+    pass
--- batch/views.py
+++ batch/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, response, permissions
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, response, permissions, generics
 
 from . import serializers, models
 
@@ -19,3 +20,30 @@ class ProjectViewset(viewsets.ViewSet):
         queryset = models.Project.objects.filter(file__owner=request.user)
         serializer = serializers.ProjectSerializer(queryset, many=True)
         return response.Response(serializer.data)
+
+
+class BatchJobViewset(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request):
+        from uuid import uuid4
+
+        serializer = serializers.BatchJob(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = request.user
+
+        models.Job.objects.create(owner=user, external_id=uuid4().hex)
+        return response.Response(serializer.data)
+
+
+class JobDoneView(generics.UpdateAPIView):
+    def update(self, request, external_id: str):
+        serializer = serializers.JobUpdate(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        job = get_object_or_404(models.Job, external_id=external_id)
+        job.status = serializer.data["status"]
+        job.save()
+
+        return response.Response(status=204)
