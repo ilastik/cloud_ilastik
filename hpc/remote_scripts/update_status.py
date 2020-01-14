924455227f463cf18513021cdddae840d7c48372
Emil Melnikov <emilmelnikov@gmail.com>
2020-01-14 15:22:22 +0100

    Extract job status update into a Python script

---
+++ hpc/remote_scripts/update_status.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""Update the current job status by sending an HTTP request."""
+
+import argparse
+import json
+import pathlib
+import urllib.request
+import urllib.parse
+
+# FIXME: The account name can change.
+ACCOUNT = "AUTH_2dc0b65279674a42833a064ce3677297"
+
+
+def parse_args():
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument(
+        "endpoint", help="base URL for reporting the job status, without the job ID, with the trailing slash"
+    )
+    ap.add_argument("job", help="unicore job ID")
+    ap.add_argument("status", help="job status", choices="created running done failed".split())
+    ap.add_argument(
+        "--output",
+        metavar="DIR",
+        help="local, top-level N5 directory with results; required when status is 'done'",
+        type=pathlib.Path,
+    )
+    ap.add_argument(
+        "--bucket", metavar="NAME", help="remote bucket name with the output contents; required when status is 'done'"
+    )
+
+    args = ap.parse_args()
+    if args.status == "done" and not all((args.output, args.bucket)):
+        ap.error("'--output' and '--bucket' are required when status is 'done'")
+
+    return args
+
+
+def main():
+    args = parse_args()
+    data = {"status": args.status}
+
+    if args.status == "done":
+        with open(args.output / "exported_data" / "attributes.json") as fd:
+            attrs = json.loads(fd)
+        data = {
+            **data,
+            "result_url": f"https://object.cscs.ch/v1/{ACCOUNT}/{args.bucket}/{args.output.name}",
+            "name": args.output.name,
+            "dtype": attrs["dataType"],
+            **{f"size_{k}": v for k, v in zip(attrs["axes"], attrs["dimensions"])},
+        }
+
+    urllib.request.urlopen(
+        urllib.request.Request(
+            urllib.parse.urljoin(args.endpoint, args.job),
+            data=json.dumps(data).encode(),
+            headers={"Content-Type": "application/json"},
+        ),
+        timeout=10,
+    )
+
+
+if __name__ == "__main__":
+    main()
