560ef3f2a6fab97f3e18d1433fc8245862059bb9
Maksim Novikov <mnovikov.work@gmail.com>
2020-01-16 16:37:40 +0100

    Isolate neuroglancer to separate module

--- cloud_ilastik/datasets/admin.py
+++ cloud_ilastik/datasets/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from .models import Dataset
 
+
 @admin.register(Dataset)
 class DatasetAdmin(admin.ModelAdmin):
-    list_display = "name url owner is_public dtype size_t size_z size_y size_x size_c".split()
-    search_fields = "name url".split()
+    list_display = ["name", "url", "owner", "is_public", "dtype", "size_t", "size_z", "size_y", "size_x", "size_c"]
+    search_fields = ["name", "url"]
