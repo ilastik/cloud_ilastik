4de78fd0a378e4302c055d7f8a1ddafef68bbace
Tomaz Vieira <tom.fogaca@gmail.com>
2020-02-19 17:37:40 +0100

    Fixes datasource download cornercases

--- hpc/remote_scripts/utils.sh
+++ hpc/remote_scripts/utils.sh
@@ -7,11 +7,26 @@ if [ -n "${HPC_PATH_PREFIX:-}" ]; then
     export PATH="$HPC_PATH_PREFIX:$PATH"
 fi
 
-# FIXME: this is very specific to our current setup
+# FIXME: this is very specific to our current setup, and should probably be done by ilastik itself
 download_datasource(){
-    BUCKET_NAME="${2:-datasources}"
+    DATASET_URL="$1"
+
+    SWIFT_AUTH_COMPONENT="$(echo $DATASET_URL | grep -Eo 'AUTH_\w+/' || echo 'NOT_FOUND')"
+    if [ "$SWIFT_AUTH_COMPONENT" = NOT_FOUND ]; then
+        FILENAME="$(random_filename)_$(last_path_component "$DATASET_URL")"
+        wget "$DATASET_URL" -O "$FILENAME"
+        echo "$FILENAME"
+        return 0
+    fi
+    GUESSED_BUCKET_NAME="$(echo "$DATASET_URL" | awk -F $SWIFT_AUTH_COMPONENT '{print $2}' | cut -d / -f 1)"
+    BUCKET_NAME="${2:-$GUESSED_BUCKET_NAME}"
     PREFIX="$(echo $1 | awk --field-separator $BUCKET_NAME/ '{print $2}' | sed 's@\.n5\.tar$@.n5@')"
     OUTPUT_DIR=$(random_filename)
+
+    if [ -z "$PREFIX" ]; then
+        echo "Trying to download the whole bucket!!" >&2
+        return 1
+    fi
 
     srun --ntasks 1 swift --quiet download -p "$PREFIX" -D $OUTPUT_DIR "$BUCKET_NAME"
     if echo "$PREFIX" | grep -Eq '\.n5$' ; then
@@ -26,8 +41,12 @@ find_n5_dataset(){
     find "$N5_DIR_PATH" | grep -E '\.n5/[^/]+/attributes.json' | head -n1 | sed 's@/attributes.json@@'
 }
 
+last_path_component(){
+    echo $1 | rev | cut -d/ -f1 | rev
+}
+
 random_filename(){
-    mktemp -u | rev | cut -d/ -f1 | rev
+    last_path_component "$(mktemp -u)"
 }
 
 USE_MPIRUN="${USE_MPIRUN:-false}"
