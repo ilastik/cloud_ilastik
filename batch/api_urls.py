dd8bcef06530967ccdfa31510cdde8f23d14d746
Maksim Novikov <mnovikov.work@gmail.com>
2020-03-23 16:56:55 +0100

    Add job done tests

--- batch/api_urls.py
+++ batch/api_urls.py
@@ -8,4 +8,4 @@ router.register("projects", views.ProjectViewset, basename="project")
 router.register(r"projects/(?P<project_id>.+)/jobs", views.JobViewset, basename="job")
 router.register("batch_jobs", views.BatchJobViewset, basename="batch_job")
 
-urlpatterns = [path("jobs/external/<str:external_id>/", views.JobDoneView.as_view())] + router.urls
+urlpatterns = [path("jobs/external/<str:external_id>/", views.JobDoneView.as_view(), name="job-done")] + router.urls
