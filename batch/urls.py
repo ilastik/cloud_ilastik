010fe1539533f5165be9806ec32787438b3f3ece
Maxim Novikov <mnovikov.work@gmail.com>
2020-03-31 14:09:11 +0200

    Add public projects (#63)
    * Add public projects
    
    * Fix template error
    
    * Allow access to detail page for public projects

--- batch/urls.py
+++ batch/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "batch"
 urlpatterns = [
-    path("projects/", views.ProjectListView.as_view(), name="project-list"),
+    path("projects/", views.project_list_page, name="project-list"),
     path("projects/<uuid:pk>/jobs/", views.ProjectDetailView.as_view(), name="project-detail"),
     path("projects/<uuid:pk>/jobs/new/", views.ProjectNewJobView.as_view(), name="project-new-job"),
 ]
